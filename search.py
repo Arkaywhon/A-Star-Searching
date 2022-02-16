0a208a8d704f2506e005197a1e37ed209c87f20d
Ryo Kinjo <ryokinjo@cmail.carleton.ca>
2022-02-16 12:49:50 -0500

    Implemented A* search function to find path from a start to end point in a map.

--- search.py
+++ search.py
@@ -1,3 +1,5 @@
+from queue import PriorityQueue
+
 def read_map_from_file(file_name):
     """
     Creates a 2D list map by reading a map from a text file.
@@ -32,7 +34,7 @@ def print_map(map):
         print()
 
 
-def get_adjacent_options(x, y, map):
+def get_adjacent_options(current_position, map):
     """
     Gets a list of adjacent tile positions that can
     be traversed to from a specified position.
@@ -41,6 +43,8 @@ def get_adjacent_options(x, y, map):
     :param map: The tile map.
     :return: A list of (x, y) tuple tile positions adjacent to the specified position.
     """
+
+    x, y = current_position
 
     map_height = len(map)
     map_width = len(map[0])
@@ -72,6 +76,66 @@ def get_adjacent_options(x, y, map):
     return options
 
 
+def manhattan_distance(position_1, position_2):
+    x1, y1 = position_1
+    x2, y2 = position_2
+    return abs(x1 - x2) + abs(y1 - y2)
+
+
+def assemble_path(goal, previous):
+    current_pos = goal
+    path = [current_pos]
+
+    while current_pos in previous:
+        current_pos = previous[current_pos]
+        path.append(current_pos)
+
+    path.reverse()
+    return path
+
+
+def find_path(start, goal, map):
+
+    tile_costs = {
+        '1': 1,
+        '2': 2
+    }
+
+    frontier = PriorityQueue()
+    frontier.put((0, start))
+
+    reached = {start: 0}
+    previous = {}
+
+    while frontier.queue:
+        priority_value, position = frontier.get()
+        path_cost = reached[position]
+
+        if position == goal:
+            return assemble_path(goal, previous)
+
+        for adjacent_position in get_adjacent_options(position, map):
+
+            adj_x, adj_y = adjacent_position
+            tile = map[adj_y][adj_x]
+
+            adjacent_path_cost = tile_costs[tile] + path_cost
+            heuristic = manhattan_distance(adjacent_position, goal)
+            priority = adjacent_path_cost + heuristic
+
+            if (adjacent_position not in reached) or (adjacent_path_cost < reached[adjacent_position]):
+
+                frontier.put((priority, adjacent_position))
+                reached[adjacent_position] = adjacent_path_cost
+                previous[adjacent_position] = position
+
+    return []
+
+
 map = read_map_from_file('map01.txt')
 print_map(map)
-print(get_adjacent_options(5, 2, map))
+
+start = (2, 2)
+goal = (5, 7)
+
+print(find_path(start, goal, map))
